3b70a941eb1cd6a21b9e69283c1f215155a753ac
lgmcharlie <87636734+lgmcharlie@users.noreply.github.com>
2021-07-19 20:05:06 +0800

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,13 @@
+  #ifndef _SORT_H                                                                                                                                                                   
+   #define _SORT_H
+   void bubbleSort(int *h,unsigned int len);
+   void insertSort(int *h,unsigned int len);
+   void bucketSort(int *h,int len);
+   void shellSort(int *h,unsigned int len);
+   void quickSort(int *h,int left,int right);
+   void heap(int *a,int len);
+   void selectSort(int *a,int len);
+   void merge(int* a,int left,int right);
+  //void heapSort(int *a,int left,int right);
+  #endif
+
